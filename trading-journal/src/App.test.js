76b7b9359937b63bde89968b235c3d51bf7d7c1e
Wide97 <mwidesott@gmail.com>
2024-11-18 16:48:34 +0100

    LoginPage

--- trading-journal/src/App.test.js
+++ trading-journal/src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import Login from './components/Login';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ trading-journal/src/components/PageLogin.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/PageLogin.css'
+
+function PageLogin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h1>TradeLogix</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default PageLogin;
