facefa45e8aa56a886d8f5d363127805135a1181
Wide97 <mwidesott@gmail.com>
2024-11-26 11:46:22 +0100

    UserJournal upgrade

--- trading-journal/src/components/userjournal/UserJournal.jsx
+++ trading-journal/src/components/userjournal/UserJournal.jsx
@@ -1,230 +1,229 @@
-// Journal.jsx
 import React, { useState } from "react";
-import { createTrade } from "../utils/apiJournal"; // Importa la logica dal file apiJournal
-import "./UserJournal.css"; // Aggiungeremo eventuali stili personalizzati
+import { createTrade } from "../utils/apiJournal";
+import "./UserJournal.css";
 import FooterPage from "../footer/FooterPage";
+import UserNav from "../usernav/UserNav"; 
 
-const Journal = () => {
-  const [tradeData, setTradeData] = useState({
-    purchaseDate: "",
-    saleDate: "",
-    purchaseTime: "",
-    saleTime: "",
-    positionSize: "",
-    leverage: "",
-    strategy: "",
-    tradeType: "LONG", // Default to LONG
-    openingCosts: "",
-    closingCosts: "",
-    result: "PROFIT", // Default to PROFIT
-    asset: "EUR/USD", // Default to EUR/USD
-  });
+const UserJournal = () => {
 
-  const [message, setMessage] = useState("");
+  const [purchaseDate, setPurchaseDate] = useState("");
+  const [saleDate, setSaleDate] = useState("");
+  const [purchaseTime, setPurchaseTime] = useState("");
+  const [saleTime, setSaleTime] = useState("");
+  const [positionSize, setPositionSize] = useState("");
+  const [leverage, setLeverage] = useState("");
+  const [strategy, setStrategy] = useState("");
+  const [tradeType, setTradeType] = useState("LONG"); 
+  const [openingCosts, setOpeningCosts] = useState("");
+  const [closingCosts, setClosingCosts] = useState("");
+  const [result, setResult] = useState("PROFIT"); 
+  const [asset, setAsset] = useState("EUR_USD"); 
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setTradeData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+
+  const resetForm = () => {
+    setPurchaseDate("");
+    setSaleDate("");
+    setPurchaseTime("");
+    setSaleTime("");
+    setPositionSize("");
+    setLeverage("");
+    setStrategy("");
+    setTradeType("LONG");
+    setOpeningCosts("");
+    setClosingCosts("");
+    setResult("PROFIT");
+    setAsset("EUR_USD");
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+
+    const tradeData = {
+      purchaseDate,
+      saleDate,
+      purchaseTime,
+      saleTime,
+      positionSize,
+      leverage,
+      strategy,
+      tradeType,
+      openingCosts,
+      closingCosts,
+      result,
+      asset,
+    };
+
     try {
-      const data = await createTrade(tradeData); // Usa la funzione di apiJournal
-      setMessage("Trade creato con successo!");
+      
+      const response = await createTrade(tradeData);
+
+      if (response.message) {
+        alert(response.message);
+        resetForm(); 
+      }
     } catch (error) {
-      setMessage("Errore nella creazione del trade.");
+      alert("Errore nella creazione del trade");
+      console.error(error);
     }
   };
 
   return (
     <>
-    <div className="container mt-5">
-      <div className="text-center">
-        <h2>Inserisci un Nuovo Trade</h2>
-        <p>Compila i campi per registrare un nuovo trade nel tuo Journal</p>
+      <UserNav />
+      <h2 className="text-center text-white mt-5">Creazione Trade</h2>
+      <div className="user-journal-container">
+        <form onSubmit={handleSubmit} className="user-journal-form">
+          <div className="form-group">
+            <label>Data di Acquisto</label>
+            <input
+              type="date"
+              value={purchaseDate}
+              onChange={(e) => setPurchaseDate(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Data di Vendita</label>
+            <input
+              type="date"
+              value={saleDate}
+              onChange={(e) => setSaleDate(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Ora di Acquisto</label>
+            <input
+              type="time"
+              value={purchaseTime}
+              onChange={(e) => setPurchaseTime(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Ora di Vendita</label>
+            <input
+              type="time"
+              value={saleTime}
+              onChange={(e) => setSaleTime(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Lottaggio della Posizione</label>
+            <input
+              type="number"
+              value={positionSize}
+              onChange={(e) => setPositionSize(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Leva</label>
+            <input
+              type="text"
+              value={leverage}
+              onChange={(e) => setLeverage(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Strategia</label>
+            <input
+              type="text"
+              value={strategy}
+              onChange={(e) => setStrategy(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Tipo di Trade</label>
+            <select
+              value={tradeType}
+              onChange={(e) => setTradeType(e.target.value)}
+              className="form-control"
+              required
+            >
+              <option value="LONG">LONG</option>
+              <option value="SHORT">SHORT</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label>Costi di Apertura</label>
+            <input
+              type="number"
+              value={openingCosts}
+              onChange={(e) => setOpeningCosts(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Costi di Chiusura</label>
+            <input
+              type="number"
+              value={closingCosts}
+              onChange={(e) => setClosingCosts(e.target.value)}
+              className="form-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Esito del Trade</label>
+            <select
+              value={result}
+              onChange={(e) => setResult(e.target.value)}
+              className="form-control"
+              required
+            >
+              <option value="PROFIT">Profitto</option>
+              <option value="STOP_LOSS">Stop Loss</option>
+              <option value="BREAK_EVEN">Break Even</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label>Asset</label>
+            <select
+              value={asset}
+              onChange={(e) => setAsset(e.target.value)}
+              className="form-control"
+              required
+            >
+              <option value="EUR_USD">EUR/USD</option>
+              <option value="BTC_USD">BTC/USD</option>
+              <option value="GOLD_USD">GOLD/USD</option>
+              <option value="SP500">SP500</option>
+              {/* Aggiungi altre opzioni di asset qui */}
+            </select>
+          </div>
+
+          <button type="submit" className="btn btn-primary mt-3">
+            Crea Trade
+          </button>
+        </form>
       </div>
-
-      {message && <div className="alert alert-info">{message}</div>}
-
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="purchaseDate" className="form-label">Data di Acquisto</label>
-          <input
-            type="date"
-            className="form-control"
-            id="purchaseDate"
-            name="purchaseDate"
-            value={tradeData.purchaseDate}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="saleDate" className="form-label">Data di Vendita</label>
-          <input
-            type="date"
-            className="form-control"
-            id="saleDate"
-            name="saleDate"
-            value={tradeData.saleDate}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="purchaseTime" className="form-label">Ora di Acquisto</label>
-          <input
-            type="time"
-            className="form-control"
-            id="purchaseTime"
-            name="purchaseTime"
-            value={tradeData.purchaseTime}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="saleTime" className="form-label">Ora di Vendita</label>
-          <input
-            type="time"
-            className="form-control"
-            id="saleTime"
-            name="saleTime"
-            value={tradeData.saleTime}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="positionSize" className="form-label">Lottaggio della Posizione</label>
-          <input
-            type="number"
-            className="form-control"
-            id="positionSize"
-            name="positionSize"
-            value={tradeData.positionSize}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="leverage" className="form-label">Leva</label>
-          <input
-            type="text"
-            className="form-control"
-            id="leverage"
-            name="leverage"
-            value={tradeData.leverage}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="strategy" className="form-label">Strategia</label>
-          <input
-            type="text"
-            className="form-control"
-            id="strategy"
-            name="strategy"
-            value={tradeData.strategy}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="tradeType" className="form-label">Tipo di Trade</label>
-          <select
-            className="form-select"
-            id="tradeType"
-            name="tradeType"
-            value={tradeData.tradeType}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="LONG">LONG</option>
-            <option value="SHORT">SHORT</option>
-          </select>
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="openingCosts" className="form-label">Costi di Apertura</label>
-          <input
-            type="number"
-            className="form-control"
-            id="openingCosts"
-            name="openingCosts"
-            value={tradeData.openingCosts}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="closingCosts" className="form-label">Costi di Chiusura</label>
-          <input
-            type="number"
-            className="form-control"
-            id="closingCosts"
-            name="closingCosts"
-            value={tradeData.closingCosts}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="result" className="form-label">Esito del Trade</label>
-          <select
-            className="form-select"
-            id="result"
-            name="result"
-            value={tradeData.result}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="PROFIT">Profit</option>
-            <option value="STOP LOSS">Stop Loss</option>
-            <option value="BREAK EVEN">Break Even</option>
-          </select>
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="asset" className="form-label">Asset</label>
-          <select
-            className="form-select"
-            id="asset"
-            name="asset"
-            value={tradeData.asset}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="EUR/USD">EUR/USD</option>
-            <option value="BTC/USD">BTC/USD</option>
-            <option value="ETH/USD">ETH/USD</option>
-            <option value="GOLD/USD">GOLD/USD</option>
-            {/* Aggiungi gli altri asset qui */}
-          </select>
-        </div>
-
-        <button type="submit" className="btn btn-primary">Crea Trade</button>
-      </form>
-    </div>
-    <FooterPage/>
+      <FooterPage />
     </>
   );
 };
 
-export default Journal;
-
+export default UserJournal;
--- trading-journal/src/components/utils/apiJournal.js
+++ trading-journal/src/components/utils/apiJournal.js
@@ -1,36 +1,30 @@
 export const createTrade = async (tradeData) => {
-    const token = localStorage.getItem("token");
-  
-    const mappedTradeData = {
-      ...tradeData,
-      result: tradeData.result.toUpperCase().replace(" ", "_"), 
-      asset: tradeData.asset.replace("/", "_"), 
-    };
-  
-    try {
-      const response = await fetch("http://localhost:3001/api/trades/createTrade", {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(mappedTradeData),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Errore nella creazione del trade: ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-      console.log('Trade creato:', data); 
-      return data;
-  
-    } catch (error) {
-      console.error('Errore nel frontend:', error);
+  const token = localStorage.getItem("token");
+  try {
+    const response = await fetch("http://localhost:3001/api/trades/createTrade", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(tradeData),
+    });
+
+    if (!response.ok) {
       throw new Error("Errore nella creazione del trade");
     }
-  };
-  
+
+    // Assicurati che la risposta sia in formato JSON
+    const data = await response.json();
+
+    return data;  // La risposta JSON contiene ora i dati (message, tradeId, ecc.)
+  } catch (error) {
+    console.error("Errore nella creazione del trade:", error);
+    throw error;
+  }
+};
+
+
   
 
 
